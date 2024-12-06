3d8fbd581517b5ddb9bd1e37444344568f7bdbaf
PaNDa2Code <moaaz0688@gmail.com>
2024-12-06 11:26:49 +0200

    0.1.0

--- build.zig
+++ build.zig
@@ -7,14 +7,11 @@ pub fn build(b: *std.Build) void {
 
     const zbench = b.dependency("zbench", .{});
 
-    const lib = b.addSharedLibrary(.{
-        .name = "zmph",
+    const module = b.addModule("zmph", .{
         .root_source_file = b.path("src/minimal_perfect_hash.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    b.installArtifact(lib);
 
     const test_step = b.step("test", "Run unit tests");
     const benchmark_step = b.step("benchmark", "Run benchmarks");
@@ -29,13 +26,13 @@ pub fn build(b: *std.Build) void {
         .name = "benchmark",
         .root_source_file = b.path("src/benchmark.zig"),
         .target = target,
-        .optimize = .ReleaseFast,
+        .optimize = optimize,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(unit_tests);
     test_step.dependOn(&run_lib_unit_tests.step);
 
-    lib_benchmark.root_module.addImport("zmph", &lib.root_module);
+    lib_benchmark.root_module.addImport("zmph", module);
     lib_benchmark.root_module.addImport("zbench", zbench.module("zbench"));
     const run_lib_benchmark = b.addRunArtifact(lib_benchmark);
     benchmark_step.dependOn(&run_lib_benchmark.step);
